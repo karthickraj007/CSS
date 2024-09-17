58cb76f8d2c604f6bcd73f22f416df8efbedeb4c
karthik raj <karthickraj.s2000@gmail.com>
2024-09-17 06:32:48 +0530

    new changes

---
+++ Notes/index.js
@@ -0,0 +1,50 @@
+/*
+
+1. What is HTML?
+    HTML stands for HyperText Markup Language. It is used to design web pages using a markup language.
+
+2.Block-Level Elements
+    1)A block-level element always starts on a new line.
+    2)it takes full width.
+
+3.Inline Elements
+    An inline element does not start on a new line and only takes up as much width as necessary.
+
+4.id attributes
+    The id attribute is used to uniquely identify an element
+
+5.class attribute
+    class attribute is used to group elements that share a common styling or behaviour
+
+6.padding
+    Padding is the space inside an element
+
+7.margin
+    margin is the space outside of it.
+
+8.Scss
+    1)it is super set of css.
+    2)It introduces additional syntax like variables, nesting, Inheritance,Functions and Operations Loops and Conditionals
+
+9.Types of css style.
+    1)inline style
+    2)external style
+    3)internal style
+
+10.absoulte
+    1)it is used to move element from parent(not staic), not affecting other elements.
+    2)no space is created for the element in the page layout.
+
+11.realtive
+    it Allows you to move an element from its original position while keeping its space in the layout.
+
+12.void element
+    area , base , br , col , command , embed , hr , img , input , keygen , link , meta , param , source , track , wbr.
+
+13.<!DOCTYPE html>
+      It tells the web browser which version of HTML you are using so it can display the page correctly.
+
+
+
+
+*/
