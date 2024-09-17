d933c647d5871f30b122cf743f65d9728bc741ab
karthik raj <karthickraj.s2000@gmail.com>
2024-09-17 07:38:12 +0530

    new changes

--- Notes/index.js
+++ Notes/index.js
@@ -1,50 +1,78 @@
 /*
 
 1. What is HTML?
-    HTML stands for HyperText Markup Language. It is used to design web pages using a markup language.
+    HTML stands for HyperText Markup Language. It is used to design web pages.
 
-2.Block-Level Elements
+2.what is css?
+    1)CSS is a computer language
+    2)it stands for Cascading style sheets.
+    3)it used to define styling and layout of web pages
+
+3.what is javascript?
+    JavaScript is a programming language that adds dynamic features to web pages, such as interactivity and animation.
+
+4.Block-Level Elements
     1)A block-level element always starts on a new line.
     2)it takes full width.
 
-3.Inline Elements
+5.Inline Elements
     An inline element does not start on a new line and only takes up as much width as necessary.
 
-4.id attributes
+6.id attributes
     The id attribute is used to uniquely identify an element
 
-5.class attribute
+7.class attribute
     class attribute is used to group elements that share a common styling or behaviour
 
-6.padding
-    Padding is the space inside an element
+8.padding
+    space between the content inside an element and its border.
 
-7.margin
-    margin is the space outside of it.
+9.margin
+    space outside an element's border.
 
-8.Scss
+10.Scss
     1)it is super set of css.
     2)It introduces additional syntax like variables, nesting, Inheritance,Functions and Operations Loops and Conditionals
 
-9.Types of css style.
+11.Types of css style.
     1)inline style
     2)external style
     3)internal style
 
-10.absoulte
+12.absoulte
     1)it is used to move element from parent(not staic), not affecting other elements.
     2)no space is created for the element in the page layout.
 
-11.realtive
+13.realtive
     it Allows you to move an element from its original position while keeping its space in the layout.
 
-12.void element
+14.void element
     area , base , br , col , command , embed , hr , img , input , keygen , link , meta , param , source , track , wbr.
 
-13.<!DOCTYPE html>
+15.<!DOCTYPE html>
       It tells the web browser which version of HTML you are using so it can display the page correctly.
 
+16.what is flex?
+    it is used to create wepage design layout.
 
+17.Box Model
+    1)Every Html element is a rectangular box.
+    2)it has five properties
+        1)height
+        2)width
+        3)border
+        4)padding
+        5)border
+
+18.box-sizing
+    it sets how the total width and height of an element are calculated.
+     1)content-box(default)
+        The width and height properties include the content, but they do not include the padding, border, or margin
+    2)border-box:
+        The width and height properties include the content, padding, and border, but they do not include the margin.
+
+19.Universal Selector (*)
+    Selects all the HTML elements within the document.
 
 
 */
